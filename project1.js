0cf72923de7ffef21d339013852b1266c35cc2c6
Siddhartha1385 <144987008+Siddhartha1385@users.noreply.github.com>
2025-06-19 06:59:46 +0000

    Done

--- project1.js
+++ project1.js
@@ -2,6 +2,10 @@ const buttons=document.querySelectorAll(".button")
 const body=document.querySelector("body")
 
 buttons.forEach((button)=>{
+  button.style.backgroundColor=button.id 
+})
+
+buttons.forEach((button)=>{
   button.addEventListener('click',(e)=>{
     body.style.backgroundColor=e.target.id;
   })
